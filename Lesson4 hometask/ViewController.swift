0b01c3dfdb21a92ee45f026bb7df1288c26abe41
4er <vishnekidze@gmail.com>
2021-08-01 20:34:05 +0300

    Initial Commit

---
+++ Lesson4 hometask/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Lesson4 hometask
+//
+//  Created by Егор Вишняков on 1.08.21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
