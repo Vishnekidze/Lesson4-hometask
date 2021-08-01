bb8bad2cdb110a9e06c8b22247c7f75a75233440
4er <vishnekidze@gmail.com>
2021-08-01 20:35:27 +0300

    .

--- Lesson4 hometask/ViewController.swift
+++ Lesson4 hometask/ViewController.swift
@@ -11,6 +11,22 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        var justCortege:[(number: Int, letter: String)] = [(1,"x"), (3,"z"), (2, "y"), (4, "a")]
+        
+        var numberSquared = justCortege.map { (number: Int, letter: String) in
+            return ((number * number), letter)
+        }
+        
+        var squaredAndFilteredNumber = numberSquared.filter { (number: Int, letter: String) in
+            return number % 2 == 0
+        }
+        squaredAndFilteredNumber.sort { firstElement, secondElement in
+            return secondElement.0 < firstElement.0
+            
+        }
+        
+        
         // Do any additional setup after loading the view.
     }
 
